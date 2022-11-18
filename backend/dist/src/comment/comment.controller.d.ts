68ca1d563878bfcbec86dc524879cbf3aaba5576
László Sipula <sipulalaci@gmail.com>
2022-11-18 23:33:36 +0100

    feat: remove dist from repo

--- backend/dist/src/comment/comment.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from 'prisma/prisma.service';
-export declare class CommentController {
-    private readonly prisma;
-    constructor(prisma: PrismaService);
-    createComment(newComment: any): Promise<import(".prisma/client").Comment>;
-    updateComment(id: any, updatedComment: any): Promise<import(".prisma/client").Comment>;
-}
--- backend/dist/src/user/user.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from '../../prisma/prisma.service';
-export declare class UserController {
-    private readonly prisma;
-    constructor(prisma: PrismaService);
-    createUser(newUser: any): Promise<import(".prisma/client").User>;
-    getUser(id: string): Promise<import(".prisma/client").User>;
-    updateUser(id: any, updatedUser: any): Promise<import(".prisma/client").User>;
-}
